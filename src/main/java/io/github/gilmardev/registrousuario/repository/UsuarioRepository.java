7cda1ac06bf73004c77648bc1afaec3a0969d77d
Gilmar Cruz Silva <gilmarcs1@hotmail.com>
2021-02-12 20:03:28 -0300

    implementação do repositório

--- src/main/java/io/github/gilmardev/registrousuario/repository/UsuarioRepository.java
+++ src/main/java/io/github/gilmardev/registrousuario/repository/UsuarioRepository.java
@@ -1,5 +1,11 @@
 package io.github.gilmardev.registrousuario.repository;
 
-public interface UsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.github.gilmardev.registrousuario.dto.UsuarioDTO;
+
+public interface UsuarioRepository extends JpaRepository<UsuarioDTO, Long> {
+	
+	UsuarioDTO findByNome(String nome);
 
 }
