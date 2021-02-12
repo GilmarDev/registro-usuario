8c501822ce29ed76239d8e9f4c989c95e7cc2c7f
Gilmar Cruz Silva <gilmarcs1@hotmail.com>
2021-02-12 19:31:45 -0300

    inicializado o projeto

---
+++ src/main/java/io/github/gilmardev/registrousuario/SistemaRegistroUsuarioApplication.java
@@ -0,0 +1,13 @@
+package io.github.gilmardev.registrousuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaRegistroUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaRegistroUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/gilmardev/registrousuario/SistemaRegistroUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gilmardev.registrousuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaRegistroUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
