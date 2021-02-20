fffac534f450e5ecc746a6624b8efaadcd02d327
Gilmar Cruz Silva <gilmarcs1@hotmail.com>
2021-02-20 05:42:43 -0300

    validação de erro

---
+++ src/main/java/io/github/gilmardev/registrousuario/exception/ErroValidacaoCampo.java
@@ -0,0 +1,36 @@
+package io.github.gilmardev.registrousuario.exception;
+
+import java.awt.TrayIcon.MessageType;
+
+//Este DTO contém as informações de um único erro de validação.
+public class ErroValidacaoCampo {
+
+	private String arquivado;
+	private String mensagem;
+	private MessageType tipo;
+
+	public String getArquivado() {
+		return arquivado;
+	}
+
+	public void setArquivado(String arquivado) {
+		this.arquivado = arquivado;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+
+	public MessageType getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(MessageType tipo) {
+		this.tipo = tipo;
+	}
+
+}
